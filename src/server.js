c94af07a0ba5c5b63dd80d65afd696d9d208ca2c
Sanford <Glenn.Sanford@pepsico.com>
2019-11-25 13:23:41 -0600

    Setup app for Heruku

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000  //setup port
 const publicDirectoryPath = path.join (__dirname, '../public')
 const viewsPath = path.join (__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
-const bodyParser = require('body-parser')
+//const bodyParser = require('body-parser')
 
 require('./db/mongoose')
 
@@ -25,53 +25,53 @@ hbs.registerPartials(partialsPath)
 app.use(express.static(publicDirectoryPath))
 
 // create application/x-www-form-urlencoded parser 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+//var urlencodedParser = bodyParser.urlencoded({ extended: false })
  
 //call json to parse data
 app.use(express.json())
 app.use(userRouter)
-app.use(sapnumRouter)
+//app.use(sapnumRouter)
 
 
 
 //*********************ROUTES SITES****************
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'SAP FIELD Support',
+        title: '',
         name: 'Glenn Sanford'
     })
 })
 
-app.get('/saphome', (req, res) => {
-    res.render('index', {
-        title: 'SAP FIELD Support',
-        name: 'Glenn Sanford'
-    })
-})
+// app.get('/saphome', (req, res) => {
+//     res.render('index', {
+//         title: 'SAP FIELD Support',
+//         name: 'Glenn Sanford'
+//     })
+// })
 
-app.get('/sap-sr', function(req,res){
-    res.render('sap-sr', {
-        title: 'SAP Script Runner',
-        name: 'Glenn Sanford'
-    })
-})
+// app.get('/sap-sr', function(req,res){
+//     res.render('sap-sr', {
+//         title: 'SAP Script Runner',
+//         name: 'Glenn Sanford'
+//     })
+// })
 
 // POST /login gets urlencoded bodies
-app.post('/sap-sr', urlencodedParser, function (req, res) {
+// app.post('/sap-sr', urlencodedParser, function (req, res) {
 
-    const fs = require('fs')
-    fs.writeFileSync('../sap-web/vbs/sap/zi01/sapSrc.txt',req.body.sapnum)
-    console.log('Complete')
+//     const fs = require('fs')
+//     fs.writeFileSync('../sap-web/vbs/sap/zi01/sapSrc.txt',req.body.sapnum)
+//     console.log('Complete')
 
-        'use strict';
+//         'use strict';
     
-        const spawn = require( 'child_process' ).spawnSync,
+//         const spawn = require( 'child_process' ).spawnSync,
        
-        vbs = spawn('cscript.exe',['../sap-web/vbs/sap/zi01/fixETMStaging.vbs', 'one' ] )
+//         vbs = spawn('cscript.exe',['../sap-web/vbs/sap/zi01/fixETMStaging.vbs', 'one' ] )
         
-        res.send('IDOC was submitted')
+//         res.send('IDOC was submitted')
 
-})
+// })
 
       
 //listen for app on port 3000
